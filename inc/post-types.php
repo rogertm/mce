a37a4729a548ac615bc9b1020cc4d1e2fc5c559b
RogerTM <roger213tm@gmail.com>
2018-09-09 15:48:39 -0400

    Agregada la taxonomía "Grado de Dificultad" para el post type Rutas

--- inc/post-types.php
+++ inc/post-types.php
@@ -84,6 +84,52 @@ function mce_post_types(){
 
 		register_post_type( $pt['post-type'], $args );
 	endforeach;
+
+	// Register Custom Taxonomies
+	$taxonomies = array(
+		'degree'	=> array(
+			'post_type'				=> array( 'route' ),
+			'singular'				=> _x( 'Degree of Difficulty', 'taxonomy singular name', 'mce' ),
+			'plural'				=> _x( 'Degrees of Difficulty', 'taxonomy general name', 'mce' ),
+			'hierarchical'			=> true,
+			'show_ui'				=> true,
+			'show_admin_column'		=> true,
+			'update_count_callback'	=> '_update_post_term_count',
+			'query_var'				=> true,
+			'rewrite'				=> array( 'slug' => 'degree' ),
+		),
+	);
+
+	foreach ( $taxonomies as $taxonomy => $tax ) :
+		$labels = array(
+			'name'					=> $tax['plural'],
+			'singular_name'			=> $tax['singular'],
+			'search_items'			=> sprintf( __( 'Search %s', 'mce' ), $tax['plural'] ),
+			'popular_items'			=> sprintf( __( 'Popular %s', 'mce' ), $tax['plural'] ),
+			'all_items'				=> sprintf( __( 'All %s', 'mce' ), $tax['plural'] ),
+			'parent_item'			=> sprintf( __( 'Parent %s', 'mce' ), $tax['plural'] ),
+			'parent_item_colon'		=> sprintf( __( 'Parent %s', 'mce' ), $tax['plural'] ),
+			'edit_item'				=> sprintf( __( 'Edit %s', 'mce' ), $tax['plural'] ),
+			'update_item'			=> sprintf( __( 'Update %s', 'mce' ), $tax['singular'] ),
+			'view_item'				=> sprintf( __( 'View %s', 'mce' ), $tax['singular'] ),
+			'add_new_item'			=> sprintf( __( 'Add New %s', 'mce' ), $tax['singular'] ),
+			'new_item_name'			=> sprintf( __( 'New %s name', 'mce' ), $tax['singular'] ),
+			'not_found'				=> sprintf( __( 'No %s found', 'mce' ), $tax['plural'] ),
+			'menu_name'				=> $tax['plural'],
+		);
+
+		$args = array(
+			'hierarchical'				=> $tax['hierarchical'],
+			'labels'					=> $labels,
+			'show_ui'					=> $tax['show_ui'],
+			'show_admin_column'			=> $tax['show_admin_column'],
+			'update_count_callback'		=> $tax['update_count_callback'],
+			'query_var'					=> $tax['query_var'],
+			'rewrite'					=> $tax['rewrite'],
+		);
+
+		register_taxonomy( $taxonomy, $tax['post_type'], $args );
+	endforeach;;
 }
 add_action( 'init', 'mce_post_types' );
 
