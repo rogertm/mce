aebfac3ecfdd3ff9542bb73f44aca3cc6b55493c
RogerTM <roger213tm@gmail.com>
2018-09-21 16:41:14 -0400

    Soporte para post type FAQ

---
+++ page-templates/mce-faq-template.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * MCE
+ *
+ * @package			WordPress
+ * @subpackage		MCE
+ * @author			RogerTM
+ * @license			license.txt
+ * @link			https://excursionismocuba.com
+ * @since 			MCE 1.0
+ */
+
+/**
+ * Template Name: MCE FAQ Page
+ */
+
+get_header(); ?>
+
+		<section id="main-content" <?php t_em_breakpoint( 'main-content' ); ?>>
+			<section id="content" role="main" <?php t_em_breakpoint( 'content-one-column' ); ?>>
+			<?php do_action( 't_em_action_content_before' ); ?>
+
+			<?php
+			// Custom query for post type FAQ
+			$args = array(
+				'post_type'			=> 'faq',
+				'posts_per_page'	=> -1,
+				'order'				=> 'ASC',
+				'orderby'			=> 'menu_order',
+			);
+
+			$faqs = get_posts( $args );
+			?>
+			<h2 id="faq-index"><?php _e( 'Table of Content', 'mce' ) ?></h2>
+			<ol class="list-unstyled">
+			<?php foreach ( $faqs as $faq ) : ?>
+				<li class="h5"><a href="<?php echo get_permalink( t_em( 'page_faq' ) ) .'#faq-'. $faq->ID ?>"><?php echo $faq->post_title ?></a></li>
+			<?php endforeach; wp_reset_postdata(); ?>
+			</ol>
+
+			<?php foreach ( $faqs as $faq ) : ?>
+			<div id="faq-<?php echo $faq->ID ?>" class="faq mb-3 border-bottom">
+				<div class="faq-body">
+					<h2 class="faq-title pt-5"><?php echo $faq->post_title ?></h2>
+					<div class="faq-text">
+						<?php echo t_em_wrap_paragraph( $faq->post_content ) ?>
+						<p class="text-right"><a href="#faq-index" class="btn btn-sm btn-link"><?php _e( 'Back to top', 'mce' ) ?></a></p>
+					</div>
+				</div>
+			</div>
+			<?php endforeach; wp_reset_postdata(); ?>
+
+			<?php do_action( 't_em_action_content_after' ); ?>
+			</section><!-- #content -->
+		</section><!-- #main-content .rwo-fluid -->
+
+<?php get_footer(); ?>
